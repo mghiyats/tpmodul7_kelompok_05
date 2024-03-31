fa98b0fc1dcd7a2d1bd737e4795f5591328b1239
zidan <zidansivafajar23@gmail.com>
2024-04-01 00:03:20 +0700

    menambahkan class DataMahasiswa_1302223039

--- tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System.Text.Json;
+using tpmodul7_kelompok_05;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataMahasiswa_1302223039.ReadJSON();
     }
+
 }
