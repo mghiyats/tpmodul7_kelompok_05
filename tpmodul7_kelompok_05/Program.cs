67af2b0c660b5539fb457bf23003eec41724efd4
rafif purnomo <rafif.purnomo@gmail.com>
2024-03-31 23:49:45 +0700

    menambahkan class DataMahasiswa_1302220003

--- tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/Program.cs
@@ -1,7 +1,9 @@
-﻿internal class Program
+﻿using tpmodul7_kelompok_05;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataMahasiswa_1302220003.ReadJSON();
     }
 }
