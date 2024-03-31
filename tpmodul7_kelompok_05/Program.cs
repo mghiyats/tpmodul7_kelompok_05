979ad6d17ec7e227580c1e80a49b9a698a01bd8e
zidan <zidansivafajar23@gmail.com>
2024-04-01 01:27:33 +0700

    menambahkan class KuliahMahasiswa_1302223039

--- tpmodul7_kelompok_05/Program.cs
+++ tpmodul7_kelompok_05/Program.cs
@@ -6,6 +6,8 @@ internal class Program
     private static void Main(string[] args)
     {
         DataMahasiswa_1302223039.ReadJSON();
+        KuliahMahasiswa_1302223039 kuliahMahasiswa = new KuliahMahasiswa_1302223039();
+        kuliahMahasiswa.ReadJSON();
     }
 
 }
